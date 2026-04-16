06b10e77e1d3ef295523142148d0042666f6cd99
iamargus95 <kamathsuraj95@gmail.com>
2026-04-17 03:53:28 +0530

    feat: migrate adguard pve2 -> pve1 (10G disk), add config export/restore

--- terraform/adguard.tf
+++ terraform/adguard.tf
@@ -3,16 +3,16 @@ module "adguard" {
 
   vmid         = 100
   hostname     = "adguard"
-  target_node  = "pve2"
+  target_node  = "pve1"
   cores        = 1
   memory       = 512
-  disk_size    = 2
+  disk_size    = 10
   storage_pool = "local-lvm"
   template_id  = var.template_id
   bridge       = var.bridge
   dns_servers  = var.dns_servers
   password     = var.container_password
-  pve_ssh_host = var.pve2_ssh_host
+  pve_ssh_host = var.pve1_ssh_host
 
   extra_lxc_config = []
 }
